174177f011263cf8876cc1246b98afdec24d3d13
Roman <vladimirovrm@gmail.com>
2022-12-13 13:29:40 +0300

    create sliders

---
+++ src/js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+	$('.carousel__inner').slick({
+		speed: 1000,
+		adaptiveHeight: true,
+		prevArrow:
+			'<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+		nextArrow:
+			'<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+		response: [
+			{
+				breakpoint: 768,
+				settings: {
+					dots: true,
+					arrows: false,
+				},
+			},
+		],
+	});
+});
