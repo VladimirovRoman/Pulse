816c8814e990a2daf4e2813eb955ab90d9ed070c
Roman <vladimirovrm@gmail.com>
2022-12-14 10:18:15 +0300

    create tabs

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 	$('.carousel__inner').slick({
 		speed: 1000,
-		adaptiveHeight: true,
+		adaptiveHeight: false,
 		prevArrow:
 			'<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
 		nextArrow:
@@ -16,4 +16,24 @@ $(document).ready(function () {
 			},
 		],
 	});
+
+	(function ($) {
+		$(function () {
+			$('ul.catalog__tabs').on(
+				'click',
+				'li:not(.catalog__tab_active)',
+				function () {
+					$(this)
+						.addClass('catalog__tab_active')
+						.siblings()
+						.removeClass('catalog__tab_active')
+						.closest('div.container')
+						.find('div.catalog__content')
+						.removeClass('catalog__content_active')
+						.eq($(this).index())
+						.addClass('catalog__content_active');
+				}
+			);
+		});
+	})(jQuery);
 });
